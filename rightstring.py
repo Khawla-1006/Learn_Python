695411dd4b38f351c738b8245c63f6e5c32529d6
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-10 17:17:42 +0100

    framed word

--- rightstring.py
+++ rightstring.py
@@ -7,7 +7,35 @@
 # Please type in a string: python
 # **************python
 
-
+# solution:
 string_input = input("Please type in a string: ")
 star = 20 - len(string_input)
 print("*" * star + string_input)
+
+
+
+
+
+# Part 2 : framed word 
+# Word: testing
+
+# ******************************
+# *          testing           *
+# ******************************
+
+# string_input = input("Please type in a string: ")
+# print("*" * 30)
+# print("*" + string_input.center(28) + "*")
+# print("*" * 30)
+
+
+#without center()
+word = input("word: ")
+print("*" * 30)
+spaces_at_start = (28-len(word)) // 2 
+spaces_at_end = spaces_at_start
+
+if len(word) % 2 != 0:
+    spaces_at_end += 1 
+print("*" + spaces_at_start * " " + word + spaces_at_end * " " + "*")
+print("*" * 30)
