89b6ac01f7106dc45c132ae3a807e02a5204de7c
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-11 16:09:54 +0100

    my solution and i'm proud of it!!

---
+++ findsubstring.py
@@ -0,0 +1,23 @@
+string = input("Please type in a word: ")
+char = input("please type in a character: ")
+
+# The program then prints the first three character slice
+# which begins with the character specified by the user. 
+
+# if char in string : 
+#     index = string.find(char)
+#     output = string[index : index + 3]
+#     if len(output) == 3 :
+#         print(output)   
+i = 0
+while True :
+    if len(string) == 0 :
+        break
+    if char in string :
+        index = string.find(char)
+        output = string[index : index + 3]
+        if len(output) == 3 : 
+            print(output)
+    i = index + 1
+    string = string[i :]
+
