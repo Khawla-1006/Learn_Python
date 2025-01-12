53a851654dc12f156953f7dbd30c4d7a9ebd962d
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-12 12:19:15 +0100

    occurence do not overlap (update)

--- secondoccurence.py
+++ secondoccurence.py
@@ -6,7 +6,9 @@ occurrence_index = []
 while index + len(sub) <= len(string) :
     if string[index : index + len(sub)] == sub :
         occurrence_index.append(index)
-    index += 1
+        index = index + len(sub)
+    else : 
+        index += 1
 
 if len(occurrence_index) >= 2 and occurrence_index[1] != -1 : 
     print("The second occurrence of the substring is at index ", occurrence_index[1]) 
