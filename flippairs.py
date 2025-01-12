ba4bf21998af9f718a2daaef0945087a6faea6ed
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-12 21:23:22 +0100

    instructions

--- flippairs.py
+++ flippairs.py
@@ -1,3 +1,9 @@
+# Please write a program which asks the user to type in a number.
+# The program then prints out all the positive integer values from 1 up to the number.
+# However, the order of the numbers is changed so that each pair or numbers is flipped. 
+# That is, 2 comes before 1, 4 before 3 and so forth.
+
+
 number = int(input("Please type in a number:"))
 
 i = 2 
