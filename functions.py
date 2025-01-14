ffe55bcfe89359deae3e9d13e7d9f7c6eb5f4bcd
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-14 22:21:19 +0100

    squared string modified ver

--- functions.py
+++ functions.py
@@ -37,9 +37,38 @@ def chessboard(a) :
         j += 1
     while i < a :
         if i % 2 == 0 :
-            print(zero)
-        else : 
             print(one)
+        else : 
+            print(zero)
+        i += 1
+
+#simpler solution :
+
+def chessboarded(size):
+    i = 0
+    while i < size:
+        if i % 2 == 0:
+            row = "10"*size
+        else:
+            row = "01"*size
+
+        # Remove extra characters at the end of the row
+        print(row[0:size])
+        i += 1
+
+def squared(word, a) : 
+    i = 0 
+    while i < a :
+        if i % 2 == 0 :
+            string = word
+        else : 
+            #reversed string
+            j = len(word) - 1
+            string = ""
+            while j > 0 :
+                string += word[j] 
+                j -= 1
+        print(string[0 : a])
         i += 1
 
 
@@ -48,5 +77,7 @@ if __name__ == "__main__" :
     # mean(3,4,5)
     # print_many_times("hi", 6)
     # hash_square(5)
-    chessboard(10)
+    # chessboard(10)
+    # chessboarded(3)
+    squared("khawla", 3)
 
