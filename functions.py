ed36b2829ab0cc44c97f757653befb4004500842
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-14 21:37:32 +0100

    chessboard

--- functions.py
+++ functions.py
@@ -21,11 +21,32 @@ def hash_square(x) :
     while i < x :
         print("#" * x)
         i += 1
-
+    
+def chessboard(a) : 
+    i = 0
+    j = 1 
+    zero = "0"
+    one = "1"
+    while j < a :
+        if j % 2 == 0 :
+            zero += "0"
+            one += "1"
+        else :
+            zero += "1"
+            one += "0"
+        j += 1
+    while i < a :
+        if i % 2 == 0 :
+            print(zero)
+        else : 
+            print(one)
+        i += 1
 
 
 if __name__ == "__main__" :
     # seven_brothers()
     # mean(3,4,5)
     # print_many_times("hi", 6)
-    hash_square(5)
+    # hash_square(5)
+    chessboard(10)
+
